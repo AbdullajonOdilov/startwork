4aa0636bf53342556766f938731f41bbea4c7d69
Abdullajon <odilovabdullajon0@gmail.com>
2024-04-27 16:04:15 +0500

    Static files

--- portfolio/views.py
+++ portfolio/views.py
@@ -4,11 +4,11 @@ from .models import Portfolio, Testimonials, Stats
 def home(request):
     portfolios = Portfolio.objects.all()
     testimonials = Testimonials.objects.all()
-    stats = Stats.objects.first()  # Assuming you have only one Stats object
+
 
     context = {
         'portfolios': portfolios,
         'testimonials': testimonials,
-        'stats': stats,
+
     }
     return render(request, 'portfolio/index.html', context)
